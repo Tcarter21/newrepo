69a47f5c1ab7e22089c09eef211372bbd70bcc61
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-10-30 20:20:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import Card from './index.js';
 function App() {
     return (
         <div className="App">
-            <Navbar>Navbar</Navbar>
             <h1>CS 230L</h1>
             <h2>Section-02</h2>
             <p>WVU ID: 800367910</p>
             <p>Hi I am Theodore</p>
-            <Card>Card 01</Card>
-            <Card>Card 02</Card>
-            <Card>Card 03</Card>
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Card 01</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
         </div>
     );
 }
