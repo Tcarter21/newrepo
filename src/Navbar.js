7cb5edff14d4798150ba1262a0ec9d698643d90c
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-10-31 19:33:28 -0400

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+function Navbar() {
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container-fluid">
+            <h1>Navbar</h1>
+            <div class="collapse navbar-collapse" id="mynavbar">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <h1>Home</h1>
+                    </li>
+                    <li class="nav-item">
+                        <h1>About</h1>
+                    </li>
+                    <li class="nav-item">
+                        <h1>Contact</h1>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="text" placeholder="Search">
+                        <button class="btn btn-primary" type="button">Search</button>
+                    </input>
+                </form>
+            </div>
+        </div>
+    </nav>
+}
