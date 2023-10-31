a782f1b188282425e9203d09671dd15a884ee22e
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-10-31 19:28:22 -0400

    Add files via upload

---
+++ Card.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+import './App.css';
+function Card() {
+    <div class="card">
+        <div class="card-body">
+            <h3 class="card-title">Card 01</h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="card-body">
+            <h3 class="card-title">Card 02</h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="card-body">
+            <h3 class="card-title">Card 03</h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div>
+}
