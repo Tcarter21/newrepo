d409dd72f5c95f66bf4b528971d1697417d792a4
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-10-30 20:06:32 -0400

    lab 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 import './App.css';
+import Navbar from './index.js';
+import Card from './index.js';
 
 function App() {
     return (
         <div className="App">
+            <Navbar>Navbar</Navbar>
             <h1>CS 230L</h1>
             <h2>Section-02</h2>
             <p>WVU ID: 800367910</p>
             <p>Hi I am Theodore</p>
+            <Card>Card 01</Card>
+            <Card>Card 02</Card>
+            <Card>Card 03</Card>
         </div>
     );
 }
