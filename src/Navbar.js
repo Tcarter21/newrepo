a42e46978b6d30ac34916448432429ecf7192e5d
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-10-31 22:15:09 -0400

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,25 +2,25 @@
 import './App.css';
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <div className="container-fluid">
-                <h1>Navbar</h1>
+        <nav className="navbar navbar-expand-md bg-dark">
+            <div className="container-fluid text-light">
+                <span className="navbar-brand text-light">Navbar</span>
                 <div className="collapse navbar-collapse" id="mynavbar">
                     <ul className="navbar-nav me-auto">
-                        <li className="nav-item">
-                            <h1>Home</h1>
+                        <li className="nav-item text-light">
+                            Home
                         </li>
-                        <li className="nav-item">
-                            <h1>About</h1>
+                        <li className="nav-item text-light">
+                            About
                         </li>
-                        <li className="nav-item">
-                            <h1>Contact</h1>
+                        <li className="nav-item text-light">
+                            Contact
                         </li>
                     </ul>
                     <form className="d-flex">
-                        <input className="form-control me-2" type="text" placeholder="Search">
-                            <button className="btn btn-primary" type="button">Search</button>
+                        <input className="form-control me-2" type="text" placeholder="Search"> 
                         </input>
+                        <button className="btn btn-primary" type="button">Search</button>
                     </form>
                 </div>
             </div>
