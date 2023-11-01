907ae5a3fe612711f05038adad9484a76a2a0dcd
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-10-31 20:33:51 -0400

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,28 +1,31 @@
 
 import './App.css';
 function Navbar() {
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <div class="container-fluid">
-            <h1>Navbar</h1>
-            <div class="collapse navbar-collapse" id="mynavbar">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <h1>Home</h1>
-                    </li>
-                    <li class="nav-item">
-                        <h1>About</h1>
-                    </li>
-                    <li class="nav-item">
-                        <h1>Contact</h1>
-                    </li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="text" placeholder="Search">
-                        <button class="btn btn-primary" type="button">Search</button>
-                    </input>
-                </form>
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container-fluid">
+                <h1>Navbar</h1>
+                <div className="collapse navbar-collapse" id="mynavbar">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <h1>Home</h1>
+                        </li>
+                        <li className="nav-item">
+                            <h1>About</h1>
+                        </li>
+                        <li className="nav-item">
+                            <h1>Contact</h1>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="text" placeholder="Search">
+                            <button className="btn btn-primary" type="button">Search</button>
+                        </input>
+                    </form>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    );
 }
+
 export default Navbar;
