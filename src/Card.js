9e6769338feefc054e1ca150b313bc7e4d40b3a1
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-10-31 20:32:59 -0400

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,21 @@
-// JavaScript source code
+
 import './App.css';
 function Card() {
-    <div class="card">
-        <div class="card-body">
-            <h3 class="card-title">Card 01</h3>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h3 className="card-title">Card 01</h3>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div className="card-body">
+                <h3 className="card-title">Card 02</h3>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div className="card-body">
+                <h3 className="card-title">Card 03</h3>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
         </div>
-        <div class="card-body">
-            <h3 class="card-title">Card 02</h3>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        <div class="card-body">
-            <h3 class="card-title">Card 03</h3>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div>
+    );
 }
 export default Card;
