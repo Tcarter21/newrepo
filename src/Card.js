3225dc3ac0baf2def3586fa162920384fdeb4948
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-10-31 22:15:46 -0400

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -2,19 +2,34 @@
 import './App.css';
 function Card() {
     return (
-        <div className="card">
-            <div className="card-body">
-                <h3 className="card-title">Card 01</h3>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="row">
+
+            <div className="col-sm-4">
+                <div className="card bg-primary text-light">
+                    <div className="card-body">
+                        <h3 className="card-title">Card 01</h3>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
             </div>
-            <div className="card-body">
-                <h3 className="card-title">Card 02</h3>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div className="card-body">
-                <h3 className="card-title">Card 03</h3>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
+
+                <div className="col-sm-4">
+                    <div className="card bg-danger text-light">
+                        <div className="card-body">
+                            <h3 className="card-title">Card 02</h3>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-sm-4">
+                    <div className="card bg-success text-light">
+                        <div className="card-body">
+                            <h3 className="card-title">Card 03</h3>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
         </div>
     );
 }
