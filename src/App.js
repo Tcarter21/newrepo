3faea9956a997d97d80c7f11d4e83b86fe2d1ebd
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-11-10 14:05:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import Navbar from './Navbar.js';
 import Card from './Card.js';
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import Home from './Home.js';
+import Contact from './Contact.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
-            <h1>CS 230L</h1>
-            <h2>Section-02</h2>
-            <p>WVU ID: 800367910</p>
-            <p>Hi I am Theodore</p>
-            <Card />
+            <div className="App">
+                <BrowserRouter>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/card" element={<Card />}></Route>
+                        <Route path="/contact" element={<Contact />}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </div>
         </div>
     );
 }
