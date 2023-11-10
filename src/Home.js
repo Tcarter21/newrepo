78925e73cbbd38febab034a6400d76169caf0dfc
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-11-10 14:09:01 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Home() {
             <h1>CS 230L</h1>
             <h2>Section-02</h2>
             <p>WVU ID: 800367910</p>
-            <p>Hi I am Theodore</p>);
+            <p>Hi I am Theodore</p>
         </div>
     );
 }
