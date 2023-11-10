debe6f697c4ee775a68ac7d81d4ac6b7e59efc3e
Tcarter21 <143536844+Tcarter21@users.noreply.github.com>
2023-11-10 14:35:43 -0500

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,29 @@ import './App.css';
 
 function Contact() {
     return (
-        <h1>Contact Us</h1>
+        <div className="Contact">
+            <h1>Contact Us</h1>
+            <div class="input-group mb-3">
+                <span class="input-group-text">First Name</span>
+                <input type="text" class="form-control" placeholder="Enter your First Name"></input>
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text">Last Name</span>
+                <input type="text" class="form-control" placeholder="Enter your Last Name"></input>
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text">Phone Number</span>
+                <input type="text" class="form-control" placeholder="Enter your Phone Number"></input>
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text">Email</span>
+                <input type="text" class="form-control" placeholder="Enter your Email"></input>
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text">Message</span>
+                <input type="text" class="form-control" placeholder="Enter your message"></input>
+            </div>
+        </div>
     );
 }
 export default Contact;
